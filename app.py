d9f5e713c7ee8da868a06dfaa921ae6f5a1ff235
magarrajan605@gmail.com <magarrajan605@gmail.com>
2025-12-24 11:37:39 +0545

    fixing

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ def clean_cache():
         subprocess.call("rm -rf /tmp", shell=True)
 # Command injection
 def weak_hash(data):
-        return hashlib.md5(data.encode()).
-        hexdigest() # Broken crypto
+        return hashlib.md5(data.encode()).hexdigest() # Broken crypto
 if __name__ == "__main__":
         print("PKI Tool Running...")
